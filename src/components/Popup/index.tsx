dc3cfcadd5bf27f1f598cdf17a52d7b13395f793
guisantanaa <santanagui40@gmail.com>
2022-07-15 12:12:47 -0300

    adjust modal

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -10,27 +10,29 @@ export default function Popup() {
   closeElement === false ? document.body.classList.add("removeModal") : null;
 
   return (
-    <div className="popup_modal">
-      <div className="close_modal">
-        <button onClick={closeOrHide}>FECHAR</button>
-      </div>
-
-      <div className="content_modal">
-        <div className="image_modal">
-          <img src={imageModal} alt="image modal gym" />
+    <div id="modal_login">
+      <div className="popup_modal">
+        <div className="close_modal">
+          <button onClick={closeOrHide}>FECHAR</button>
         </div>
 
-        <div className="form_modal">
-          <EnvelopeSimple size={32} />
-          <span>Bem Vindo à MAEZTRA</span>
-          <p>Receba em Primeira mão </p>
-          <strong>desconto e ofertas exclusivas</strong>
-          <form>
-            <input type="email" placeholder="Digite seu e-mail" />
-            <button>
-              ENVIAR <PaperPlaneTilt size={20} />
-            </button>
-          </form>
+        <div className="content_modal">
+          <div className="image_modal">
+            <img src={imageModal} alt="image modal gym" />
+          </div>
+
+          <div className="form_modal">
+            <EnvelopeSimple size={32} />
+            <span>Bem Vindo à MAEZTRA</span>
+            <p>Receba em Primeira mão </p>
+            <strong>desconto e ofertas exclusivas</strong>
+            <form>
+              <input type="email" placeholder="Digite seu e-mail" />
+              <button>
+                ENVIAR <PaperPlaneTilt size={20} />
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
